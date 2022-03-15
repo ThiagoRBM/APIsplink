ba6223d51f58ec70bca7a38ded1a511337593d24
ThiagoRBM <52140942+ThiagoRBM@users.noreply.github.com>
2022-03-15 16:31:11 -0300

    Add files via upload

--- splinkGIT.R
+++ splinkGIT.R
@@ -6,43 +6,62 @@
 ## formato da busca por nome cientifico: https://api.splink.org.br/records/ScientificName/Dioclea%20violacea/json
 
 library(httr)
-
+library(readxl)
 
 ##### buscando dados de ocorrências das espécies no SPLink #######
-SPLink = "https://api.splink.org.br/"
-Records= "records/"
-Genero= "Genus/"
-Species= "ScientificName/"
-json= "/json"
-tab= "/tab"
-xml= "/xml"
-csv= "/csv"
-Coordenadas= "/Coordinates/yes" ## pegando só o que tem coordenadas
 
 
-nomesCientificos= c("Dioclea violacea", "Mabea fistulifera", "Hancornia speciosa") ## exemplo: rode essa linha
-## sem alterar
-
-
-Nomes= function (Nomes) {
-  Nomes= gsub(",", "/", Nomes, perl = TRUE)
-  Nomes= gsub(" ", "%20", Nomes, perl = TRUE)
-  Nomes= paste(Nomes, collapse= ",")
-  
-} ## arrumando os nomes para montar a url
-
-vetorNomes= Nomes(nomesCientificos)
-
-
-urlSPLink= paste0(SPLink,Records,Species,vetorNomes,Coordenadas,csv) ## criando a URL
-siteSPLink= httr::POST(urlSPLink, inherits=FALSE) ## fazendo o pedido
-informacoesSPLink2 = httr::content(siteSPLink, encoding = "UTF-8",
-                                   type = 'text/tab-separated-values') ## retornando os dados
+tabelaNomes= read_excel("NomesCientificos.xlsx") ## tabela do excel que tenha uma coluna com nomes cientificos
+## de interesse
 
 caminho= "C:/Users/HP/Desktop/" ## colocar o caminho do diretorio onde quer salvar a tabela do SPlink
 nomeTabela= "BuscaAPIsplink.txt" ## colocar o nome que a tabela tera
 
-write.table(informacoesSPLink2, paste0(caminho, nomeTabela), sep= "\t",
-            fileEncoding = "UTF-16LE", row.names = FALSE, col.names = TRUE) ## rodar aqui para salvar
-## o que foi baixado em uma tabela
+
+###### NAO MEXER DAQUI PARA BAIXO 
+###### SERÁ AVISADO QUANDO FOR PARA MEXER NOVAMENTE
+###### 
+###### 
+###### 
+###### 
+###### 
+
+EspeciesSPlink= function(tabela, NomesCientificos){
+
+SPLink = "https://api.splink.org.br/"
+Species= "ScientificName/"
+csv= "/csv"
+Coordenadas= "/Coordinates/yes" ## pegando só o que tem coordenadas
+  
+Nomes= tabela[[NomesCientificos]]
+  
+
+vetorNomes= Nomes %>% 
+  gsub(",", "/", ., perl = TRUE) %>% 
+  gsub(" ", "%20", ., perl = TRUE)
+
+vetorNomes= paste(vetorNomes, collapse= ",") ## arrumando os nomes para montar a url
+
+
+urlSPLink= paste0(SPLink,Records,Species,vetorNomes,Coordenadas,csv) ## criando a URL
+
+siteSPLink= httr::POST(urlSPLink, inherits=FALSE) ## fazendo o pedido
+informacoesSPLink = httr::content(siteSPLink, encoding = "UTF-8",
+                                   type = 'text/tab-separated-values') ## retornando os dados
+
+write.table(informacoesSPLink, paste0(caminho, nomeTabela), sep= "\t",
+            fileEncoding = "UTF-16LE", row.names = FALSE, col.names = TRUE) ## salvando o resultado da busca
+## no diretorio indicado no começo do script, usando o nome indicado tambem
+
+
+return(informacoesSPLink)
+
+}
+
+
+InfoEspecies= EspeciesSPlink(tabelaNomes, "nomeCientifico") 
+## substituir "tabelaNomes" pela tabela em que existe a coluna com as especies de interesse (SEM ASPAS)
+## substituir "nomeCientifico" com o nome da coluna da tabela em que estão os nomes (COM ASPAS)
+
+
 
